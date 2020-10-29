b1bede2366c26d7e3be2f2e0055a0b558f9f4a6f
Gustavo <gustavo.souza2000@outlook.com>
2020-10-29 14:19:06 -0300

    refactor: return account

--- src/data/useCases/db-add-account/db-add-account.ts
+++ src/data/useCases/db-add-account/db-add-account.ts
@@ -13,6 +13,6 @@ export class DbAddAccount implements AddAccount {
       ...accountData,
       password: hashedPassword
     })
-    return new Promise(resolve => resolve(account))
+    return account
   }
 }
