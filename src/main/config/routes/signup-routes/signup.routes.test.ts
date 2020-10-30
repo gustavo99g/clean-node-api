2297442da66c935fb2f54e796dcf36cdec6a6e11
Gustavo <gustavo.souza2000@outlook.com>
2020-10-30 11:41:40 -0300

    refactor: add mongoDb connection do signup test

--- src/main/config/routes/signup-routes/signup.routes.test.ts
+++ src/main/config/routes/signup-routes/signup.routes.test.ts
@@ -1,7 +1,20 @@
 import app from '../../app'
 import request from 'supertest'
+import { MongoHelper } from '../../../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('CORS', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    const accountCollections = MongoHelper.getCollection('accounts')
+    await accountCollections.deleteMany({})
+  })
+
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
