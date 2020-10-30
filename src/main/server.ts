5ac39acb99505c14031036e448ffdf47e8a6e3f3
Gustavo <gustavo.souza2000@outlook.com>
2020-10-30 11:58:10 -0300

    feat: connect to mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,9 @@
 
 import app from './config/app'
+import env from './config/env'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 
-app.listen(3333)
+MongoHelper.connect(env.Mongo_URL).then(() => {
+  app.listen(3333)
+})
+  .catch(console.error)
