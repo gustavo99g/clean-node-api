731898a867bbde1eeb2bcdaf3de4060bb92db971
Gustavo <gustavo.souza2000@outlook.com>
2020-11-03 11:15:37 -0300

    feat: ensure loginController returns 500 is emailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
-import { badRequest, ok } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { EmailValidator } from '../../protocols/email-validator'
 import { InvalidParamError } from '../../errors/invalid-param-error'
@@ -8,21 +8,25 @@ import { InvalidParamError } from '../../errors/invalid-param-error'
 export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { email, password } = httpRequest.body
+    try {
+      const { email, password } = httpRequest.body
 
-    if (!email) {
-      return Promise.resolve(badRequest(new MissingParamError('Email not provided')))
+      if (!email) {
+        return Promise.resolve(badRequest(new MissingParamError('Email not provided')))
+      }
+
+      if (!password) {
+        return Promise.resolve(badRequest(new MissingParamError('Password not provided')))
+      }
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return Promise.resolve(badRequest(new InvalidParamError('Email is not valid')))
+      }
+
+      return Promise.resolve(ok('success'))
+    } catch (err) {
+      return serverError(err)
     }
-
-    if (!password) {
-      return Promise.resolve(badRequest(new MissingParamError('Password not provided')))
-    }
-
-    const isValid = this.emailValidator.isValid(email)
-    if (!isValid) {
-      return Promise.resolve(badRequest(new InvalidParamError('Email is not valid')))
-    }
-
-    return Promise.resolve(ok('success'))
   }
 }
