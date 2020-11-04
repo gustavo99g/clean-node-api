132ade85f36be8cdbfb9a92ada3294c53a390a67
Gustavo <gustavo.souza2000@outlook.com>
2020-11-04 11:48:18 -0300

    test: ensure sign returna token on success

--- src/infra/crypt/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/crypt/jwt-adapter/jwt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return Promise.resolve('any_token')
+  }
+}))
+
 interface SutTypes {
   sut: JwtAdapter
 }
@@ -11,10 +17,16 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 describe('JWT adapter', () => {
-  test('should ', async () => {
+  test('should call sign with correc values ', async () => {
     const { sut } = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.generate('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+  test('should return a token on sign success', async () => {
+    const { sut } = makeSut()
+
+    const token = await sut.generate('any_id')
+    expect(token).toBe('any_token')
   })
 })
