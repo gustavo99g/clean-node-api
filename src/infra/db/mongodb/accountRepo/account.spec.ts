71af2f9b42a99cd695bd7744650d7cf36210a54a
Gustavo <gustavo.souza2000@outlook.com>
2020-11-04 13:30:13 -0300

    test: ensure account return a account if valid email provided

--- src/infra/db/mongodb/accountRepo/account.spec.ts
+++ src/infra/db/mongodb/accountRepo/account.spec.ts
@@ -1,5 +1,6 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountRepo } from './account'
+import { AddAccountModel } from '../../../../domain/useCases/add-account'
 
 describe('Account mongo repository', () => {
   beforeAll(async () => {
@@ -14,17 +15,38 @@ describe('Account mongo repository', () => {
     await accountCollections.deleteMany({})
   })
 
-  test('should return an account on success', async () => {
-    const sut = new AccountRepo()
-    const account = await sut.add({
+  const makeFakeAccount = (): AddAccountModel => {
+    return {
       name: 'any_name',
       email: 'any_email@email.com',
       password: 'any_password'
-    })
+    }
+  }
+
+  test('should return an account on add success', async () => {
+    const sut = new AccountRepo()
+    const account = await sut.add(makeFakeAccount())
     expect(account).toBeTruthy()
     expect(account.id).toBeTruthy()
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email@email.com')
     expect(account.password).toBe('any_password')
+  })
+  test('should return an account on findByEmail success', async () => {
+    const sut = new AccountRepo()
+    await sut.add(makeFakeAccount())
+    const account = await sut.findByEmail('any_email@email.com')
+    expect(account).toBeTruthy()
+    expect(account?.id).toBeTruthy()
+    expect(account?.name).toBe('any_name')
+    expect(account?.email).toBe('any_email@email.com')
+    expect(account?.password).toBe('any_password')
+  })
+
+  test('should return null if findByEmail dont find a account', async () => {
+    const sut = new AccountRepo()
+
+    const account = await sut.findByEmail('invalid_email@email.com')
+    expect(account).toBeNull()
   })
 })
