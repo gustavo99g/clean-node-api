fead8828da26d35242ea1e2c0bf6c467512df25c
Gustavo <gustavo.souza2000@outlook.com>
2020-11-05 14:13:15 -0300

    feat: ensure dbAddAccount calls findByEmailRepo with correct email"

--- src/data/useCases/db-add-account/db-add-account.ts
+++ src/data/useCases/db-add-account/db-add-account.ts
@@ -2,11 +2,18 @@ import { AddAccount, AddAccountModel } from '../../../domain/useCases/add-accoun
 import { AccountModel } from '../../../domain/models/account'
 import { Hasher } from '../../protocols/crypto/hasher'
 import { AddAccountRepo } from '../../protocols/db/add-account-repo'
+import { findByEmailRepo } from '../../protocols/db/find-by_email-repo'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly hasher: Hasher, private readonly addAccountRepo: AddAccountRepo) {}
+  constructor (
+    private readonly hasher: Hasher,
+    private readonly addAccountRepo: AddAccountRepo,
+    private readonly findByEmailRepo: findByEmailRepo
+  ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.findByEmailRepo.findByEmail(accountData.email)
+
     const hashedPassword = await this.hasher.Hash(accountData.password)
 
     const account = await this.addAccountRepo.add({
