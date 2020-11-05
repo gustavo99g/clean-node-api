856682f3f29a08c99799e38ea35d81bee55586d9
Gustavo <gustavo.souza2000@outlook.com>
2020-11-05 12:11:09 -0300

    feat: ensure signupController return 200 with token if valid data is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -3,12 +3,14 @@ import { badRequest, serverError, ok } from '../../helpers/http/http-helper'
 import { Controller } from '../../protocols/controller'
 import { AddAccount } from '../../../domain/useCases/add-account'
 import { Validation } from '../../protocols/validation'
+import { Authentication } from '../../../domain/useCases/authentication'
 
 export class SignUpController implements Controller {
   constructor (
 
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -18,9 +20,12 @@ export class SignUpController implements Controller {
         return badRequest(error)
       }
       const { name, email, password } = httpRequest.body
-      const account = await this.addAccount.add({ name, password, email })
-
-      return ok(account)
+      await this.addAccount.add({ name, password, email })
+      const token = await this.authentication.auth({
+        email,
+        password
+      })
+      return ok({ token })
     } catch (err) {
       return serverError(err)
     }
