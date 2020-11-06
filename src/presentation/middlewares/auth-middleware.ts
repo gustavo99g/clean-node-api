6e984a63badf02e1c481ac14904c40897bafa308
Gustavo <gustavo.souza2000@outlook.com>
2020-11-06 12:26:10 -0300

    feat: ensure authMiddleware returns 500 if findByAccessToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,23 +1,27 @@
 import { Middleware } from '../protocols/middleware'
 import { HttpRequest, HttpResponse } from '../protocols/http'
-import { ok, forbidden } from '../helpers/http/http-helper'
+import { ok, forbidden, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { findByAccessTokenRepo } from '../../data/protocols/db/find-by-access-token-repo'
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly findByAccesTokenRepo: findByAccessTokenRepo) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
 
-    if (!accessToken) {
-      return forbidden(new AccessDeniedError())
+      if (!accessToken) {
+        return forbidden(new AccessDeniedError())
+      }
+      const account = await this.findByAccesTokenRepo.find(accessToken)
+
+      if (!account) {
+        return forbidden(new AccessDeniedError())
+      }
+
+      return ok({ accountId: account.id })
+    } catch (err) {
+      return serverError(err)
     }
-    const account = await this.findByAccesTokenRepo.find(accessToken)
-
-    if (!account) {
-      return forbidden(new AccessDeniedError())
-    }
-
-    return Promise.resolve(ok({ accountId: account.id }))
   }
 }
