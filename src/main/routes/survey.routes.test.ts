a982a478ff8de72342fdc24314f2441bc6d3c29c
Gustavo <gustavo.souza2000@outlook.com>
2020-11-05 21:24:29 -0300

    feat: ensure addSurvey route return 204 on success

---
+++ src/main/routes/survey.routes.test.ts
@@ -0,0 +1,36 @@
+import app from '../config/app'
+
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+
+describe('SURVEY ROUTES', () => {
+  let surveyCollections
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollections = await MongoHelper.getCollection('survey')
+    await surveyCollections.deleteMany({})
+  })
+  test('should return 204 on success', async () => {
+    await request(app)
+      .post('/api/survey')
+      .send({
+        question: 'Qual a melhor linguagem',
+        answer: [{
+          image: 'any_image',
+          answers: 'Javascript'
+        },
+        {
+          image: 'any_image',
+          answers: 'C sharp'
+        }
+        ]
+      })
+      .expect(204)
+  })
+})
