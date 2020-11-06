41016a2e17844e11ae72e4295b18a874612aac73
Gustavo <gustavo.souza2000@outlook.com>
2020-11-06 15:13:01 -0300

    feat: ensure dbFindByAccessToken call findByAccessTokenRepo with correct value

--- src/data/useCases/findByAccessToken/db-find-by-accessToken.ts
+++ src/data/useCases/findByAccessToken/db-find-by-accessToken.ts
@@ -1,14 +1,20 @@
 import { FindByAccessToken } from '../../../domain/useCases/find-by-accessToken'
 import { AccountModel } from '../../../domain/models/account'
 import { Decrypter } from '../../protocols/crypto/token-decrypter'
+import { findByAccessTokenRepo } from '../../protocols/db/find-by-access-token-repo'
 
 export class DbFindByAccessToken implements FindByAccessToken {
-  constructor (private readonly decrypter: Decrypter) {}
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly findByAccessTokenRepo: findByAccessTokenRepo
+  ) {}
+
   async find (token: string, role?: string): Promise<AccountModel | null> {
-    const id = await this.decrypter.decrypt(token)
-    if (!id) {
+    const isValid = await this.decrypter.decrypt(token)
+    if (!isValid) {
       return null
     }
+    await this.findByAccessTokenRepo.find(token)
     return {
       id: 'any_id',
       name: 'any_name',
