10200bfe1778525488c66ef46e46292111ddb976
Gustavo <gustavo.souza2000@outlook.com>
2020-11-06 11:44:00 -0300

    test: ensure authMiddleware call findByAccessToken with correct value

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,11 +1,34 @@
 import { AuthMiddleware } from './auth-middleware'
 import { AccessDeniedError } from '../errors/access-denied-error'
 import { forbidden } from '../helpers/http/http-helper'
+import { findByAccessTokenRepo } from '../../data/protocols/db/find-by-access-token-repo'
+import { AccountModel } from '../../domain/models/account'
 
-const makeSut = (): any => {
-  const sut = new AuthMiddleware()
+const makeFindByAccessToken = (): findByAccessTokenRepo => {
+  class FindByAccessTokenStub implements findByAccessTokenRepo {
+    async find (token: string): Promise<AccountModel | null> {
+      return Promise.resolve({
+        id: 'any_id',
+        email: 'any_email',
+        name: 'any_name',
+        password: 'any_password'
+      })
+    }
+  }
+  return new FindByAccessTokenStub()
+}
 
-  return { sut }
+interface SutTypes {
+  sut: AuthMiddleware
+  findByAccessTokenStub: findByAccessTokenRepo
+}
+
+const makeSut = (): SutTypes => {
+  const findByAccessTokenStub = makeFindByAccessToken()
+
+  const sut = new AuthMiddleware(findByAccessTokenStub)
+
+  return { sut, findByAccessTokenStub }
 }
 
 describe('Auth Middleware', () => {
@@ -13,5 +36,15 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const res = await sut.handle({})
     expect(res).toEqual(forbidden(new AccessDeniedError()))
+  })
+  test('should call findByAccessToken with correct token', async () => {
+    const { sut, findByAccessTokenStub } = makeSut()
+    const findSpy = jest.spyOn(findByAccessTokenStub, 'find')
+    await sut.handle({
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    })
+    expect(findSpy).toHaveBeenCalledWith('any_token')
   })
 })
