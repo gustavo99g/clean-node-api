1e128894bdd50233a821764ac178f162cf5aee32
Gustavo <gustavo.souza2000@outlook.com>
2020-11-09 13:17:53 -0300

    test: ensure get route survey return 403 if no token provided

--- src/main/routes/survey.routes.test.ts
+++ src/main/routes/survey.routes.test.ts
@@ -20,57 +20,67 @@ describe('SURVEY ROUTES', () => {
     accountCollections = await MongoHelper.getCollection('accounts')
     await accountCollections.deleteMany({})
   })
-  test('should return 403 if user without access token', async () => {
-    await request(app)
-      .post('/api/survey')
-      .send({
-        question: 'Qual a melhor linguagem',
-        answer: [{
-          image: 'any_image',
-          answers: 'Javascript'
-        },
-        {
-          image: 'any_image',
-          answers: 'C sharp'
-        }
-        ]
+  describe('POST SURVEYS', () => {
+    test('should return 403 if user without access token', async () => {
+      await request(app)
+        .post('/api/survey')
+        .send({
+          question: 'Qual a melhor linguagem',
+          answer: [{
+            image: 'any_image',
+            answers: 'Javascript'
+          },
+          {
+            image: 'any_image',
+            answers: 'C sharp'
+          }
+          ]
+        })
+        .expect(403)
+    })
+    test('should return 204 with  valid access token', async () => {
+      const res = await accountCollections.insertOne({
+        name: 'Gustavo',
+        email: 'gustavo2@gmail.com',
+        password: 'hashPassword',
+        role: 'admin'
+
       })
-      .expect(403)
+
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, 'secret')
+
+      await accountCollections.updateOne({ _id: id }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/survey')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Qual a melhor linguagem',
+          answer: [{
+            image: 'any_image',
+            answers: 'Javascript'
+          },
+          {
+            image: 'any_image',
+            answers: 'C sharp'
+          }
+          ]
+
+        })
+        .expect(204)
+    })
   })
-  test('should return 204 with  valid access token', async () => {
-    const res = await accountCollections.insertOne({
-      name: 'Gustavo',
-      email: 'gustavo2@gmail.com',
-      password: 'hashPassword',
-      role: 'admin'
 
+  describe('GET SURVEYS', () => {
+    test('should return 403 if user without access token', async () => {
+      await request(app)
+        .get('/api/survey')
+        .expect(403)
     })
-
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, 'secret')
-
-    await accountCollections.updateOne({ _id: id }, {
-      $set: {
-        accessToken
-      }
-    })
-
-    await request(app)
-      .post('/api/survey')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Qual a melhor linguagem',
-        answer: [{
-          image: 'any_image',
-          answers: 'Javascript'
-        },
-        {
-          image: 'any_image',
-          answers: 'C sharp'
-        }
-        ]
-
-      })
-      .expect(204)
   })
 })
