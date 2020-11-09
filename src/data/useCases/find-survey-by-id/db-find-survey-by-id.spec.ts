e41cd3fa8a35a8a9cb3e88fa676dc829674d7d9a
Gustavo <gustavo.souza2000@outlook.com>
2020-11-09 16:11:53 -0300

    test: ensure dbFindByIdSurvey call findByIdSurveyRepo with correct values

---
+++ src/data/useCases/find-survey-by-id/db-find-survey-by-id.spec.ts
@@ -0,0 +1,46 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { FindByIdSurveyRepo } from '../../protocols/db/find-by-id-survey'
+import { DbFindByIdSurvey } from './db-find-survey-by-id'
+
+const makeSurveyData = (): SurveyModel => {
+  return {
+    id: 'any_id',
+    question: 'any_question',
+    answers: [
+      {
+        image: 'any_image',
+        answer: 'any_answer'
+      }
+    ],
+    date: new Date()
+  }
+}
+
+const makeFindByIdSurverRepo = (): FindByIdSurveyRepo => {
+  class FindByIdSurveyRepoStub implements FindByIdSurveyRepo {
+    async findById (id: string): Promise<SurveyModel> {
+      return Promise.resolve(makeSurveyData())
+    }
+  }
+  return new FindByIdSurveyRepoStub()
+}
+
+interface SutTypes {
+  findByIdSurveyRepoStub: FindByIdSurveyRepo
+  sut: DbFindByIdSurvey
+}
+
+const makeSut = (): SutTypes => {
+  const findByIdSurveyRepoStub = makeFindByIdSurverRepo()
+  const sut = new DbFindByIdSurvey(findByIdSurveyRepoStub)
+
+  return { sut, findByIdSurveyRepoStub }
+}
+describe('Db find by id survey', () => {
+  test('should call findBy Id repo with correct value', async () => {
+    const { findByIdSurveyRepoStub, sut } = makeSut()
+    const findSpy = jest.spyOn(findByIdSurveyRepoStub, 'findById')
+    await sut.findById('any_id')
+    expect(findSpy).toHaveBeenCalledWith('any_id')
+  })
+})
