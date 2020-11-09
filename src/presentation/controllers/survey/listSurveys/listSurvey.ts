a022384fef32fcab3cadf393f7d9634d555a071c
Gustavo <gustavo.souza2000@outlook.com>
2020-11-09 10:47:16 -0300

    feat: ensure listSurveyController returns 500 if listSurvey throws

--- src/presentation/controllers/survey/listSurveys/listSurvey.ts
+++ src/presentation/controllers/survey/listSurveys/listSurvey.ts
@@ -1,12 +1,16 @@
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { ListSurveys } from '../../../../domain/useCases/list-survey'
 
 export class ListSurveysController implements Controller {
   constructor (private readonly listSurveys: ListSurveys) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const surveys = await this.listSurveys.list()
-    return Promise.resolve(ok(surveys))
+    try {
+      const surveys = await this.listSurveys.list()
+      return Promise.resolve(ok(surveys))
+    } catch (err) {
+      return serverError(err)
+    }
   }
 }
