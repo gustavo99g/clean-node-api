69b0d57dfc27609c1d1fb08b9a8d3fb92cb68ba5
Gustavo <gustavo.souza2000@outlook.com>
2020-11-09 11:58:28 -0300

    feat: add list on surveyRepo

--- src/infra/db/mongodb/surveyRepo/survey.ts
+++ src/infra/db/mongodb/surveyRepo/survey.ts
@@ -1,10 +1,20 @@
 import { AddSurveyRepo } from '../../../../data/protocols/db/add-survey-repo'
 import { AddSurveyModel } from '../../../../domain/useCases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { listSurveysRepo } from '../../../../data/protocols/db/list-survey-repo'
+import { SurveyModel } from '../../../../domain/models/survey'
 
-export class SurveyRepo implements AddSurveyRepo {
+export class SurveyRepo implements AddSurveyRepo, listSurveysRepo {
   async add (survey: AddSurveyModel): Promise<void> {
     const surveyCollections = await MongoHelper.getCollection('surveys')
     await surveyCollections.insertOne(survey)
+  }
+
+  async list (): Promise<SurveyModel[]> {
+    const surveyCollections = await MongoHelper.getCollection('surveys')
+    const results = await surveyCollections.find().toArray()
+
+    const surveys = results.map(result => MongoHelper.map(result))
+    return surveys
   }
 }
