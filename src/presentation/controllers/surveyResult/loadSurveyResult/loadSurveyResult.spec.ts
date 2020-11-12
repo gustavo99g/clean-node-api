63b2f5e7b005d682a43d89e5e3c686712ce55908
Gustavo <gustavo.souza2000@outlook.com>
2020-11-12 13:39:18 -0300

    test: ensure loadSurveyController throw if findSurveyById throws

--- src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.spec.ts
+++ src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.spec.ts
@@ -1,7 +1,7 @@
 import { HttpRequest } from '../../../protocols/http'
 import { LoadSurveyController } from './loadSurveyResult'
 import { FindByIdSurveySpy } from '../../../test/mock-survey'
-import { forbidden } from '../../../helpers/http/http-helper'
+import { forbidden, serverError } from '../../../helpers/http/http-helper'
 import { InvalidParamError } from '../../../errors/invalid-param-error'
 
 interface SutTypes {
@@ -24,7 +24,7 @@ const fakeRequest = (): HttpRequest => {
   }
 }
 describe('load Survey Controller', () => {
-  test('should call loadSurveyById with correct values', async () => {
+  test('should call findSurveyById with correct values', async () => {
     const { sut, findSurveyByIdSpy } = makeSut()
     const findSpy = jest.spyOn(findSurveyByIdSpy, 'findById')
     await sut.handle(fakeRequest())
@@ -35,5 +35,15 @@ describe('load Survey Controller', () => {
     jest.spyOn(findSurveyByIdSpy, 'findById').mockReturnValue(Promise.resolve(null))
     const res = await sut.handle(fakeRequest())
     expect(res).toEqual(forbidden(new InvalidParamError('SurveyId')))
+  })
+  test('should throw if findSurveyById throws', async () => {
+    const { sut, findSurveyByIdSpy } = makeSut()
+    jest.spyOn(findSurveyByIdSpy, 'findById').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = fakeRequest()
+    const res = await sut.handle(httpRequest)
+    expect(res).toEqual(serverError(new Error()))
   })
 })
