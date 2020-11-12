079a3d878178a8edcba53f6831932890a0e456b0
Gustavo <gustavo.souza2000@outlook.com>
2020-11-12 14:19:21 -0300

    feat: ensure loadSurveyController calls loadSurveyResult with correct value

--- src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.ts
+++ src/presentation/controllers/surveyResult/loadSurveyResult/loadSurveyResult.ts
@@ -3,9 +3,14 @@ import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { ok, forbidden, serverError } from '../../../helpers/http/http-helper'
 import { FindSurveyByID } from '../../../../domain/useCases/find-survey-by-id'
 import { InvalidParamError } from '../../../errors/invalid-param-error'
+import { LoadSurveyResult } from '../../../../domain/useCases/load-survey-result'
 
 export class LoadSurveyController implements Controller {
-  constructor (private readonly findSurveyById: FindSurveyByID) {}
+  constructor (
+    private readonly findSurveyById: FindSurveyByID,
+    private readonly loadSurveyResult: LoadSurveyResult
+  ) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { surveyId } = httpRequest.params
@@ -13,6 +18,7 @@ export class LoadSurveyController implements Controller {
       if (!survey) {
         return forbidden(new InvalidParamError('SurveyId'))
       }
+      await this.loadSurveyResult.load(surveyId)
       return ok('ok')
     } catch (err) {
       return serverError(err)
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,9 +1,16 @@
 import { SaveSurveyResult, SaveSurveyResultModel } from '../../domain/useCases/save-survey-result'
 import { SurveyResultModel } from '../../domain/models/survey-result'
 import { mockSurveyResultModel } from '../../domain/test/mock-survey-result'
+import { LoadSurveyResult } from '../../domain/useCases/load-survey-result'
 
 export class SaveSurveyResultSpy implements SaveSurveyResult {
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    return Promise.resolve(mockSurveyResultModel())
+  }
+}
+
+export class LoadSurveyResultSpy implements LoadSurveyResult {
+  async load (surveyId: string): Promise<SurveyResultModel> {
     return Promise.resolve(mockSurveyResultModel())
   }
 }
